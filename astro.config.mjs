63cbb089d8c39273dd98293f761cd12ea6c8730a
Vendicated <vendicated@riseup.net>
2025-06-27 01:20:26 +0200

    add site to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://vesktop.vencord.dev",
+
     integrations: [
         starlight({
             title: "Vesktop",
